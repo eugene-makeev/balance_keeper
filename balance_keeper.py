934e45007f05e7b36766c901c8a8ac9d56e8a5d8
Eugene <makeev.eugene@gmail.com>
2018-11-02 22:33:43 +0300

    remove print

--- balance_keeper.py
+++ balance_keeper.py
@@ -304,7 +304,6 @@ def is_rate_changed(order):
     order_book = call_api(method="/public/getorderbook", market=order['Exchange'], type=order_type)
     if order_book['success']:
         for competitor in order_book['result']:
-            print("competitor:", competitor)
             if competitor['Rate'] != order['Limit']:
                 # TODO: ckeck if MIN_COMPETITOR_ORDER_VOLUME is suitable for altcoins
                 if competitor['Quantity'] >= MIN_COMPETITOR_ORDER_VOLUME:
